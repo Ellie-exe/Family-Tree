7b78ce823544b18c41877a1b03fad131fd1ab0ef
Ellie-exe <ryiscool247@gmail.com>
2022-10-17 10:53:38 -0400

    Implemented a basic express server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,19 @@
+const express = require('express');
+const path = require('path');
 
+const app = express();
+const PORT = 443;
+
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+app.get('/api', (req, res) => {
+    res.json({ message: 'Hello there!' });
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}!`);
+});
