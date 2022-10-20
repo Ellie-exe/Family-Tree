6f854bf72c744b327422a0fa0154a8d519dbc908
James LaFarr <lafarrjam99@gmail.com>
2022-10-20 15:05:31 -0400

    Deleted components directory; it belongs in dev branch

--- client/src/feed/Feed.js
+++ client/src/feed/Feed.js
@@ -1,29 +1,6 @@
 import React from 'react';
 
 const Feed = () => {
-    const [user, setUser] = React.useState(null);
-    const [tree, setTree] = React.useState(null);
-
-    const email = 'someone@gmail.com';
-    const trees = [];
-
-    React.useEffect(() => {
-        fetch(`/api/users/${email}`)
-            .then((res) => res.json())
-            .then((data) => setUser(data.message));
-    }, []);
-
-    for (const treeID of user.trees) {
-        React.useEffect(() => {
-            fetch(`/api/trees/${treeID}`)
-                .then((res) => res.json())
-                .then((data) => setTree(data.message));
-        }, []);
-
-        trees.push(tree);
-    }
-
     return (<h1>Feed</h1>)
 };
-
 export default Feed;
