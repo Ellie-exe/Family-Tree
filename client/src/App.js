04506796dc5481b8826b2fa2f95f33aaebd915b7
Ellie-exe <ryiscool247@gmail.com>
2022-10-23 09:38:22 -0400

    Added basic login button

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Login from "./login/Login";
+import Login from './login/Login';
 
 const App = () => {
     return (
--- client/src/login/Login.js
+++ client/src/login/Login.js
@@ -1,7 +1,47 @@
 import React from 'react';
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
+import './Login.css'
 
 const Login = () => {
-  return(<h1>Login</h1>)
+    const clientId = "186520065813-541as043cebcthf0j3571f7f575peqgv.apps.googleusercontent.com";
+
+    const [isShown, setIsShown] = React.useState(false);
+
+    const onSuccess = (res) => {
+        console.log('success:', res);
+        setIsShown(current => !current);
+    };
+
+    const onFailure = (err) => {
+        console.log('failed:', err);
+        // setIsShown(current => !current);
+    };
+
+    return(
+        <div>
+            {!isShown && (
+                <GoogleLogin className='center'
+                    clientId={clientId}
+                    buttonText="Sign in with Google"
+                    onSuccess={onSuccess}
+                    onFailure={onFailure}
+                    cookiePolicy={'single_host_origin'}
+                    isSignedIn={true}
+                />
+            )}
+
+            {isShown && (
+                <div className='center'>
+                    <h2>[Insert website here]</h2>
+                    <GoogleLogout
+                        clientId={clientId}
+                        buttonText="Logout"
+                        onLogoutSuccess={onSuccess}
+                    />
+                </div>
+            )}
+        </div>
+    )
 };
 
 export default Login;
