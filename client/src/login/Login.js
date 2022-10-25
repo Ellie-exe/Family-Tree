dd4ee743d20df48024ecbb7eb42741f8f9345b4c
James LaFarr <lafarrjam99@gmail.com>
2022-10-25 17:59:11 -0400

    User can now sign in with Google.

--- client/src/login/Login.js
+++ client/src/login/Login.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import { GoogleLogin } from 'react-google-login';
+import styles from './Login.module.css';
 
-const Login = (clientId) => {
-  return(<GoogleLogin clientId={clientId}/>)
+const Login = (props) => {
+  return(
+      <div className={styles.container}>
+        <h1>Log in to FamilTree</h1>
+        <GoogleLogin clientId={props.clientId}/>
+      </div>
+  )
 };
 
 export default Login;
