301a813483f6dba8a80dd7934e2f8e9567e43c82
James LaFarr <lafarrjam99@gmail.com>
2022-10-26 14:25:45 -0400

    Added placeholder request. Added dummy data and still need to make it look nice with some CSS.

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,11 @@
 import React from 'react';
-import './App.css';
-
-const delay = (ms) => {
-    return new Promise(
-        resolve => setTimeout(resolve, ms)
-    );
-}
+import Feed from './feed/Feed.js';
 
 const App = () => {
-    const [data, setData] = React.useState(null);
-
-    React.useEffect(() => {
-        fetch('/api')
-            .then((res) => res.json())
-            .then(async (data) => {
-                await delay(1000)
-                setData(data.message)
-            });
-    }, []);
 
     return (
         <div className='App'>
-            <header className='App-header'>
-                <p>{data ? data : 'Loading...'}</p>
-            </header>
+            <Feed/>
         </div>
     );
 }
