686a7e73a8f021ace74f53344bdfd514da7669d9
James LaFarr <lafarrjam99@gmail.com>
2022-10-26 10:40:15 -0400

    Added basic UI of feed. Still need to prettify it, and get proper API route

--- client/src/feed/Feed.js
+++ client/src/feed/Feed.js
@@ -1,6 +1,26 @@
 import React from 'react';
+import {useState, useEffect} from "react";
+import styles from './Feed.module.css';
 
 const Feed = () => {
-    return (<h1>Feed</h1>)
+    const [trees, setTrees] = useState([]);
+
+    useEffect(async () => {
+        /* need to get actual route */
+        setTrees(await fetch('http://localhost').trees)
+    }, [])
+    return (
+        <div className={styles.container}>
+            {trees.length !== 0 && trees.map(tree => {
+                return (
+                    <div>
+                        <h1>{tree.name}</h1>
+                        <h1>{tree.numOccupants}</h1>
+                    </div>
+                )
+            })}
+
+        </div>
+    )
 };
 export default Feed;
