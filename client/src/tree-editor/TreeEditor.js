6113c715f539eba7d06f429030d0a0a13c7bb05b
James LaFarr <lafarrjam99@gmail.com>
2022-10-28 21:24:51 -0400

    Added basic layout of TreeEditor component.

--- client/src/tree-editor/TreeEditor.js
+++ client/src/tree-editor/TreeEditor.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const TreeEditor = () => {
-  return(<h1>Tree editor</h1>)
+  /* Use effect block here */
+  const [tree, setTree] = useState([]);
+
+  return (<>{tree.map(member => <h1>{member.name}</h1>)}</>)
 };
 
 export default TreeEditor;
