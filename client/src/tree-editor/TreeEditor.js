e77a8ee6b68121d112fc779b6f6e0ae9f1dfbf2b
James LaFarr <lafarrjam99@gmail.com>
2022-10-31 10:18:55 -0400

    Added needed redirect for when someone clicks on the member of a tree.

--- client/src/tree-editor/TreeEditor.js
+++ client/src/tree-editor/TreeEditor.js
@@ -1,11 +1,21 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 const TreeEditor = () => {
-  /* Use effect block here */
-  const [tree, setTree] = useState([]);
-  // add on click of members
+    const history = useHistory();
+    /* Use effect block here */
+    const [tree, setTree] = useState([]);
+    // add on click of members
+    const onMember = (id) => {
+        history.push('/member/editor?memberId=' + id);
+    };
 
-  return (<>{tree.map(member => <h1>{member.name}</h1>)}</>)
+    return (
+        <div>
+            {tree.map(member => <h1 onClick={() => onMember(member.id)}>{member.name}</h1>)}
+        </div>
+    )
 };
+
 
 export default TreeEditor;
