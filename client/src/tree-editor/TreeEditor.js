16effcc5139336fa7ddc4a9a3500bca5ee8a7482
Ellie-exe <ryiscool247@gmail.com>
2022-11-02 10:37:07 -0400

    Fixed Corey's name

--- client/src/tree-editor/TreeEditor.js
+++ client/src/tree-editor/TreeEditor.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 const TreeEditor = () => {
-  const [members, setMembers] =  useState([{name: 'Jim'}, {name: 'Ryan'}, {name: 'Cory'}, {name: 'Cameron'}]);
+  const [members, setMembers] =  useState([{name: 'Jim'}, {name: 'Ryan'}, {name: 'Corey'}, {name: 'Cameron'}]);
 
   return(<>{members.map(member => <h1>{member.name}</h1>)}</>)
 };
