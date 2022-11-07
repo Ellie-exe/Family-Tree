d23d421678e71e22963fd9ea41c34c33661916b1
James LaFarr <lafarrjam99@gmail.com>
2022-11-07 11:14:20 -0500

    Added redirect when a tree is clicked on home page.

--- client/src/feed/Feed.js
+++ client/src/feed/Feed.js
@@ -1,33 +1,39 @@
 import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import styles from './Feed.module.css';
 
 const Feed = () => {
-    const [trees, setTrees] = useState([]);
+    const [trees, setTrees] = useState([{id: '4592345', numMembers: 5}]);
+    const history = useHistory();
 
-    useEffect(() => {
-        const getData = async () => {
-            try {
-                return await fetch('http://localhost:8080/api/users/trees', {
-                    headers: {
-                        'Authorization': `Bearer ${localStorage.getItem('token')}`
-                    }
-                });
-            } catch (err) {
-                console.log(err);
-            }
+    const onTreeClick = (treeId) => {
+        history.push(`/trees?treeId=${treeId}`);
+    };
 
-        };
-        setTrees(getData().trees);
-    }, [])
+    // useEffect(() => {
+    //     const getData = async () => {
+    //         try {
+    //             return await fetch('http://localhost:8080/api/users/trees', {
+    //                 headers: {
+    //                     'Authorization': `Bearer ${localStorage.getItem('token')}`
+    //                 }
+    //             });
+    //         } catch (err) {
+    //             console.log(err);
+    //         }
+    //
+    //     };
+    //     setTrees(getData().trees);
+    // }, [])
     return (
         <div className={styles.container}>
             <div>
-                {trees.length !== 0 && trees.map(tree => {
+                {trees.map(tree => {
                     return (
-                        <>
+                        <div className={styles.tree} onClick={() => onTreeClick(tree.id)}>
                             <h1>{tree.id}</h1>
                             <h1>{tree.numMembers}</h1>
-                        </>
+                        </div>
                     )
                 })}
                 <span>&#43;</span>
