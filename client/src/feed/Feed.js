4f1ba003b273162d48187578977cf4ff8adb5271
James LaFarr <lafarrjam99@gmail.com>
2022-11-08 15:52:02 -0500

    Added needed requests and form to name a newly created tree

--- client/src/feed/Feed.js
+++ client/src/feed/Feed.js
@@ -3,12 +3,45 @@ import { useHistory } from 'react-router-dom';
 import styles from './Feed.module.css';
 
 const Feed = () => {
-    const [trees, setTrees] = useState([{id: '4592345', numMembers: 5}]);
+    const [trees, setTrees] = useState([]);
+    const [nameForm, setNameForm] = useState(false);
+    const [treeName, setTreeName] = useState('');
     const history = useHistory();
 
     const onTreeClick = (treeId) => {
         history.push(`/trees?treeId=${treeId}`);
     };
+
+    const onPlusClick = () => {
+        setNameForm(true);
+    };
+
+    const nameChangeHandler = (e) => {
+      setTreeName(e.target.value);
+    };
+
+    const onNameSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            await fetch('localhost:8080/api/trees', {
+                method: 'POST',
+                headers: {
+                    'Authorization': `Bearer ${localStorage.getItem('token')}`
+                },
+                body: JSON.stringify({name: treeName})
+            })
+            setTreeName('');
+            const res = await fetch('http://localhost:8080/api/users/trees', {
+                headers: {
+                    'Authorization': `Bearer ${localStorage.getItem('token')}`
+                }
+            });
+            setTrees(res.trees);
+        } catch (e) {
+            console.log(e);
+        }
+        setNameForm(false);
+    }
 
     // useEffect(() => {
     //     const getData = async () => {
@@ -28,6 +61,9 @@ const Feed = () => {
     return (
         <div className={styles.container}>
             <div>
+                {nameForm && <form onSubmit={onNameSubmit}><input onChange={nameChangeHandler} type='text' value={treeName}/>
+                    <button type='submit'>Submit</button>
+                </form>}
                 {trees.map(tree => {
                     return (
                         <div className={styles.tree} onClick={() => onTreeClick(tree.id)}>
@@ -36,7 +72,9 @@ const Feed = () => {
                         </div>
                     )
                 })}
-                <span>&#43;</span>
+                <div onClick={onPlusClick} className={styles.tree}>
+                    <h1>+</h1>
+                </div>
             </div>
         </div>
     )
