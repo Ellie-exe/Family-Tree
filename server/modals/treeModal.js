2218dbcaa2565c7e47701867f6fa09aa06c4f611
Ellie-exe <ryiscool247@gmail.com>
2022-11-15 23:22:37 -0500

    Reverted to previous backend structure

--- server/modals/treeModal.js
+++ server/modals/treeModal.js
@@ -4,8 +4,8 @@ const { Schema } = mongoose;
 const treeSchema = new Schema({
     name: String,
     numMembers: Number,
-    generation: [[{ type: Schema.Types.ObjectId, ref: 'members' }]],
-    users: { type: Map, of: Boolean }
+    members: [{ type: Schema.Types.ObjectId, ref: 'members' }],
+    users: { type: Map, of: String }
 });
 
 export default mongoose.model('trees', treeSchema);
