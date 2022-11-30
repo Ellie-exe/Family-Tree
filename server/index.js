5c1d74ea700903eaf22901ad705a7cf7c5f2d7a2
Ellie-exe <ryiscool247@gmail.com>
2022-11-30 09:01:38 -0500

    Testing https certs

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import path from 'path';
+import fs from 'fs';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -13,6 +14,9 @@ app.use(express.static(path.join(__dirname, '../client/build')));
 app.use(cors({
     origin: 'https://familtree.xyz'
 }))
+
+let privateKey = fs.readFileSync( 'private.key.pem' );
+let certificate = fs.readFileSync( 'domain.cert.pem' );
 
 app.use(express.json());
 
@@ -28,12 +32,19 @@ import trees from './routes/trees.js';
 app.use('/api/trees', trees);
 
 import users from './routes/users.js';
+import * as https from "https";
 app.use('/api/users', users);
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}!`);
-});
+// app.listen(PORT, () => {
+//     console.log(`Server listening on port ${PORT}!`);
+// });
+
+https.createServer({
+    key: privateKey,
+    cert: certificate
+}, app).listen(PORT);
+
